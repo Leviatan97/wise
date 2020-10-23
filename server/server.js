b4963b769ecd2e597cbe3cc09c19ac03cb36aff3
Sebastian Ortegon <sebastom209@gmail.com>
2020-10-23 16:53:40 -0500

    propuesta

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const cors = require('cors')
+const socketIO = require('socket.io')
+const http = require('http')
+const path = require('path')
+
+class server {
+    app
+    server_
+    io
+    publicPath
+    constructor() {
+        this.publicPath = path.join(__dirname, 'public'); 
+        this.app = express()
+        this.server_ = http.createServer(this.app)
+        this.io = socketIO(this.server_)
+        this.initialSetup()
+        this.setPort()
+        this.listenPort()
+        this.socketSetup()
+    }
+
+    initialSetup() {
+        this.app.use(cors({ origin: true, credentials: true }))
+        this.app.use(express.json())
+        this.app.use(express.static(this.publicPath));
+    }
+
+    socketSetup() {
+        this.io.on('connection',(socket)=>{
+            console.log('conectado')
+            this.io.emit('funciona','funciona')
+        })
+    }
+
+    setPort() {
+        this.app.set('port',3000)
+    }
+
+    listenPort() {
+        const server = this.app.listen(this.app.get('port'),()=>{
+            console.log(`Server started on port ${this.app.get('port')}`)
+        })
+        return server
+    }
+}
+
+module.exports = {server}
