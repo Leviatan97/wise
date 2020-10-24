4cc101a0064399dbb7fc1799a92e4e1e002e1c08
Sebastian Ortegon <sebastom209@gmail.com>
2020-10-24 10:55:48 -0500

    server.js

--- server/server.js
+++ server/server.js
@@ -5,13 +5,18 @@ const http = require('http')
 const path = require('path')
 
 class server {
-    app = express()
-    constructor( 
-        server_ = http.createServer(this.app), 
-        io = socketIO(this.server_), 
-        publicPath = path.join(__dirname, 'public')
+    
+    constructor(
+        app, 
+        server_, 
+        io, 
+        publicPath
         ) 
     {
+        this.app = express()
+        this.server_ = http.createServer(this.app)
+        this.io = socketIO(this.server_)
+        this.publicPath = path.join(__dirname, 'public')
         this.initialSetup()
         this.setPort()
         this.listenPort()
