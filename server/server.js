02c6e41e6f7f82fa0035dd103f390f3958949901
Sebastian Ortegon <sebastom209@gmail.com>
2020-10-24 16:54:52 -0500

    server.js

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ class server {
 
     listenPort() {
         const server = this.server_.listen(3000,()=>{
-            console.log(`Server started on port ${this.app.get('port')}`)
+            console.log(`Server started on port 3000`)
         })
         return server
     }
