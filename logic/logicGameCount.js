e9ea69dcfa6a0aaa76218fb5b811965aa65ecf39
Sebastian Ortegon <sebastom209@gmail.com>
2020-11-03 10:37:08 -0500

    update logicGameCount.js

--- logic/logicGameCount.js
+++ logic/logicGameCount.js
@@ -17,9 +17,9 @@ class logicGameCount {
                 console.log('no se creo la partida')
             } else {
                 console.log(`partida generada con el socket ${socket.id}, respuesta correcta ${number}`)
-                const players = players.getPlayer(player.hostId)
-                console.log(players)
-                players.forEach(element => {
+                const players_ = players.getPlayer(player.hostId)
+                console.log(players_)
+                players_.forEach(element => {
                     if(element.onGame == false) {
                         io.to(element.playerId).emit('init-game-count',{
                             response: number
