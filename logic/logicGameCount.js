31e0803fa494240e02869424d0af04af28f5507c
Sebastian Ortegon <sebastom209@gmail.com>
2020-11-04 08:20:45 -0500

    update logicGameCount.js

--- logic/logicGameCount.js
+++ logic/logicGameCount.js
@@ -60,6 +60,7 @@ class logicGameCount {
 
     resultGameCount(socket, io) {
         return (params)=> {
+            console.log(params)
             const player = players.getPlayer(socket.id)
             const game = games.getGame(player.hostId)
             const gameCount = moduleGameCount_.getGame(game.pin)
