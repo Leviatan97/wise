3dcfe7eec53b4aa9aa145a3cbd3663ec691fd1a1
Sebastian Ortegon <sebastom209@gmail.com>
2020-11-09 10:31:53 -0500

    test position

--- logic/logicGameCount.js
+++ logic/logicGameCount.js
@@ -127,7 +127,7 @@ class logicGameCount {
         console.log(players)
         for (let index = 0; index < players.length; index++) {
             
-            if (index != 0) {
+            if (index > 0) {
                 if(players[index-1].result == players[index].result) {
                     console.log(players[index].result)
                     position = {
