1f8b5aca1734c363113bde623d0e8c48676b9620
Sebastian Ortegon <jortegon@godigital.com.co>
2020-12-15 21:42:59 -0500

    test game card count

--- socket/socketGameCardCount.js
+++ socket/socketGameCardCount.js
@@ -6,11 +6,11 @@ class socketGameCardCount {
     constructor() {}
 
     createGameCardCount(socket, io) {
-        return socket.on('create-game-card', logicGameCardCount_.createGameCardCount(socket, io))
+        return socket.on('create-game-card-count', logicGameCardCount_.createGameCardCount(socket, io))
     }
 
     resultGameCardCount(socket, io) {
-        return socket.on('result-game-card', logicGameCardCount_.resultGameCardCount(socket, io))
+        return socket.on('result-game-card-count', logicGameCardCount_.resultGameCardCount(socket, io))
     }
 
     socketsGameCardCount(socket, io) {
